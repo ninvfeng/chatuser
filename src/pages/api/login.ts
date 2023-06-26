41debebb442094a88e4acb798b129a666f2f31f6
ninvfeng <ninvfeng@gmail.com>
2023-06-26 09:54:26 +0800

    fix

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -4,7 +4,7 @@ export const post: APIRoute = async(context) => {
   const body = await context.request.json()
 
   const { email, code, shareCode } = body
-  const referer = context.request.headers.get('referer')
+  const referer = context.request.headers.get('origin')
 
   const response = await fetch(`${import.meta.env.API_URL}/login/loreg`, {
     headers: {
